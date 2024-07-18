58580a86eb7615fadcffff05e0bb4757ab91f3de
chcoco173 <chen012023@icloud.com>
2024-07-18 18:20:46 +0900

    남희님 news 컨트롤러 추가

---
+++ seoulInfo/src/main/java/com/example/controller/NewsController.java
@@ -0,0 +1,37 @@
+package com.example.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+//import com.example.service.MemberService;
+
+@Controller
+@RequestMapping("/news")
+//@SessionAttributes("mem") //Model 객체에 member라고 저장된 객체를 세션에 등록한다.
+public class NewsController {
+	
+	// 로그파일 설정
+//	static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+//	
+//	@Autowired
+//	private MemberService memberservice;
+
+	@RequestMapping("/")
+	public String index() {
+		return "/index";
+	}
+	
+	@RequestMapping("/{step}")
+	public String newsViewPage(@PathVariable String step) {
+		System.out.println(step);
+		return "news/"+step;
+	}
+
+	
+
+}
