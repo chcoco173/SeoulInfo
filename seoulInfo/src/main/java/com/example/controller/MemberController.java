c6cf6f49fa97b4ff537902f5c774269459d0017e
julia0722 <132641312+julia0722@users.noreply.github.com>
2024-07-26 14:41:09 +0900

    한남희_회원가입성공 수정

--- seoulInfo/src/main/java/com/example/controller/MemberController.java
+++ seoulInfo/src/main/java/com/example/controller/MemberController.java
@@ -68,7 +68,7 @@ public class MemberController {
         memberService.insertMember(vo);
 
         // 회원가입 성공 후 로그인 페이지로 이동
-        return "redirect:member/loginA";
+        return "redirect:/member/loginA";
     }
 	
 	
