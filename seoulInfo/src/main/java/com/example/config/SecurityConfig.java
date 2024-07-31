d62f5766a8869e7950e5ddac290bbd128ee3d144
julia0722 <132641312+julia0722@users.noreply.github.com>
2024-07-31 09:25:10 +0900

    7/30_멤버_로그인 수정

--- seoulInfo/src/main/java/com/example/config/SecurityConfig.java
+++ seoulInfo/src/main/java/com/example/config/SecurityConfig.java
@@ -19,5 +19,40 @@ public class SecurityConfig implements WebMvcConfigurer {
         return new BCryptPasswordEncoder();        
     }
 
+    
+    
 
 }
+
+//@Bean
+//public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//  http
+//      .authorizeHttpRequests(authorize -> authorize
+//          .requestMatchers("/member/**","index","/").permitAll() // "/member/**" 경로는 모두 접근 허용
+//          .anyRequest().authenticated() // 다른 요청은 인증 필요
+//      )
+//      .formLogin(formLogin -> formLogin
+//          .loginPage("/")		// 로그인 페이지 설정
+//          .permitAll()				// 로그인 페이지는 모두 접근 허용
+//      )
+//      .logout(logout -> logout
+//          .permitAll()				// 로그아웃 페이지는 모두 접근 허용
+//      )
+//      .sessionManagement(sessionManagement -> sessionManagement
+//          .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+//          .maximumSessions(1)
+//          .maxSessionsPreventsLogin(true) // 최대 세션 수 설정
+//      );
+//  return http.build();
+//}
+//
+//@Bean
+//public PasswordEncoder passwordEncoder() {
+//  return new BCryptPasswordEncoder();
+//}
+//
+//@Bean
+//public WebSecurityCustomizer webSecurityCustomizer() {
+//  return (web) -> web.ignoring().requestMatchers("/resources/**");
+//}
+
