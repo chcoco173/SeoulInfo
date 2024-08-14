bfaa39b8507fe89cef48b5dc47fa651631e9fb62
LilJiwon <yejinji7@gmail.com>
2024-08-14 20:52:51 +0900

    Create WebConfig.java

---
+++ seoulInfo/src/main/java/com/example/websocket/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // 또는 허용할 IP 주소를 명시
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
