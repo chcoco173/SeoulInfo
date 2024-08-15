25e6d7d8cdf00cd7bc0dbc7510cd20a95f1c7546
LilJiwon <yejinji7@gmail.com>
2024-08-15 22:06:02 +0900

    chat 수정 festival review update
    chat 수정 festival review update

--- seoulInfo/src/main/java/com/example/controller/NewsController.java
+++ seoulInfo/src/main/java/com/example/controller/NewsController.java
@@ -13,18 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.example.domain.NewsVO;
 import com.example.service.NewsService;
 
-//import com.example.service.MemberService;
-
 @Controller
 @RequestMapping("/news")
-//@SessionAttributes("mem") //Model 객체에 member라고 저장된 객체를 세션에 등록한다.
 public class NewsController {
-	
-	// 로그파일 설정
-//	static final Logger logger = LoggerFactory.getLogger(MemberController.class);
-//	
-//	@Autowired
-//	private MemberService memberservice;
 	
 	@Autowired
 	private NewsService newsService;
@@ -40,7 +31,7 @@ public class NewsController {
 		return "news/"+step;
 	}
 
-	// Model model 변수 이름 고정하기!
+	// 구별 뉴스 리스트 가져오기
 	@GetMapping("/news")
 	public String newsArea(@RequestParam(value = "area", required = false) String area, Model model) {
 		List<NewsVO> newsList = newsService.getNewsList(area);
